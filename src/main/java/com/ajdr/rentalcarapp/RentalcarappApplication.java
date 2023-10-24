f09c03e3668265a7a288bc0f9f206f08751ae441
Jenn <jenniferjuarezjj@gmail.com>
2023-10-24 11:12:47 -0400

    initial commit

---
+++ src/main/java/com/ajdr/rentalcarapp/RentalcarappApplication.java
@@ -0,0 +1,13 @@
+package com.ajdr.rentalcarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalcarappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalcarappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajdr/rentalcarapp/RentalcarappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajdr.rentalcarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalcarappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
